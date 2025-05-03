f5b10fac0b9f03c7fd5173d5852b40cf1469c625
Martti Malmi <sirius@iki.fi>
2025-05-03 12:18:39 +0300

    do not index nip05 beginning with npub1

--- scripts/profileIndexer.ts
+++ scripts/profileIndexer.ts
@@ -161,7 +161,7 @@ export class ProfileIndexer {
       if (!name) return;
 
       let nip05 = profile.nip05 ? (profile.nip05.split('@')[0].trim().toLowerCase().slice(0, PROFILE_NAME_MAX_LENGTH)) : undefined;
-      if (nip05 && (nip05.length === 1 || name.toLowerCase().includes(nip05))) {
+      if (nip05 && (nip05.length === 1 || nip05.startsWith("npub1") || name.toLowerCase().includes(nip05))) {
         nip05 = undefined;
       }
     
